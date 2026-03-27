ada60674b20e98774c52791f1e8049357fe1fb95
Stinnia <32062581+Stinnia@users.noreply.github.com>
2026-03-27 11:27:30 +0100

    Button margin

--- medidb/src/lib/CardOverlay.svelte
+++ medidb/src/lib/CardOverlay.svelte
@@ -1,9 +1,7 @@
 <script>
 </script>
 
-<div
-  class="bg-white w-80 items-center inline-block pt-2 pb-2 px-6 m-2 rounded-xl shadow-xl"
->
+<div class="bg-white w-full px-6 py-6 rounded-xl shadow-xl">
   <div class="block">
     <div class="text-md font-bold">
       <slot>Card title</slot>
