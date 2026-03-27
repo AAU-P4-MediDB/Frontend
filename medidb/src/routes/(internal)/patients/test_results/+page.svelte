941d0ef639e81dc299eb68d36b8b983346927a54
mollyjessing <molly@jessing.dk>
2026-03-27 13:27:14 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ medidb/src/routes/(internal)/patients/test_results/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+    import { Card } from "flowbite-svelte";
+</script>
+
+<div class="grid grid-cols-3 md:grid-cols-2 gap-4 m-4 p-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 m-4 p-4">
+        <Card href="/cards" class="p-4 sm:p-6 md:p-8">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">John Cena</h5>
+            <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Blood Test - 04/10/2026</p>
+            <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Status: Definitive</p>
+        </Card>
+        <Card href="/cards" class="p-4 sm:p-6 md:p-8">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">John Cena</h5>
+            <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Blood Test - 04/10/2026</p>
+            <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Status: Definitive</p>
+        </Card>
+        <Card href="/cards" class="p-4 sm:p-6 md:p-8">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">John Cena</h5>
+            <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Blood Test - 04/10/2026</p>
+            <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Status: Definitive</p>
+        </Card>
+        <Card href="/cards" class="p-4 sm:p-6 md:p-8">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">John Cena</h5>
+            <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Blood Test - 04/10/2026</p>
+            <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Status: Definitive</p>
+        </Card>
+    </div>
+
+    <div>
+        <Card class="grid grid-cols-2 md:grid-cols-2 col-span-2 gap-4 m-4 p-4">
+            <div>
+                <Card href="/cards" class="p-4 sm:p-6 md:p-8">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Subject</h5>
+                    <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">John Cena</p>
+                    <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">45 yrs </p>
+                    <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Male </p>
+                </Card>
+                <Card href="/cards" class="p-4 sm:p-6 md:p-8">
+                    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Test info</h5>
+                    <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Blood Test</p>
+                    <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">07/10/2026 - Definitive</p>
+                    <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">04/10/2026 - Dr. Vanna Bardot </p>
+                </Card>
+            </div>
+            <Card href="/cards" class="p-4 sm:p-6 md:p-8">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Results</h5>
+                <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget metus risus. Donec ac risus sit amet justo eleifend hendrerit. In dictum velit quis luctus accumsan. Vestibulum sit amet metus nibh. Quisque dapibus in sem non finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras ultrices mi eu tempor blandit. Nunc at elementum tortor. Nunc eu volutpat justo. Nulla cursus id sem a varius. Fusce tincidunt tempor est in scelerisque. Aenean in libero dolor. Vivamus turpis ex, ultrices vitae erat id, posuere placerat felis. Curabitur tincidunt euismod turpis. In sit amet cursus elit. Maecenas sed finibus ex.
+
+                    Sed magna odio, ullamcorper at lectus hendrerit, convallis aliquet erat. Nulla blandit justo vitae congue convallis. Morbi quis diam eu ipsum bibendum cursus. Nulla eu dapibus nisi, eu mollis urna. Duis posuere vitae dolor nec tristique. Maecenas justo tortor, sodales vel nisl et, placerat lobortis nisi. Nullam sapien neque, pretium et malesuada in, semper id nulla. Sed lacinia dolor ac aliquet euismod. Nullam felis ligula, vestibulum eget nunc et, volutpat efficitur ex.</p>
+                <p class="leading-tight font-normal text-gray-700 dark:text-gray-400">Status: Definitive</p>
+            </Card>
+        </Card>
+    </div>
+</div>
+
