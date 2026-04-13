f3076c7713cb524556269a9a547f2b4051f8e027
Voxy <development@voxvoltera.com>
2026-04-13 11:31:56 +0200

    Hooked up calendar

--- medidb-sv5/src/routes/(internal)/calendar/+page.server.ts
+++ medidb-sv5/src/routes/(internal)/calendar/+page.server.ts
@@ -1,13 +1,16 @@
 import { api } from '$lib/server/api';
 import type { PageServerLoad } from './$types';
+import { Temporal } from "temporal-polyfill";
 
-
-export interface Patients {
+export interface AppointmentEntry {
+	uuid: string;
+	cpr: string;
 	name: string;
-	pronouns: string;
-	age: number;
-	pfp: string; //path to pfp
+	reason: string;
+	time: number;
+	pfp: string;
 }
+
 
 export const load: PageServerLoad = async ({ cookies }) => {
 	const cookieHeader = cookies
@@ -18,20 +21,46 @@ export const load: PageServerLoad = async ({ cookies }) => {
 	// TODO: pull doctorID from session cookie once auth is wired up
 	const doctorID = '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'; 
 
-	let patients: Patients[] = [];
+	let appointment_data: AppointmentEntry[] = [];
+
+	const timeAddon = 1800; // 30 minutes in seconds (hardcoded, cause i can't be fucked to implement it properly)
+
 	
 	try {
-		patients = await api.get<Patients[]>(
-			`/api/dpm/pf/${doctorID}`,
+		appointment_data = await api.get<AppointmentEntry[]>(
+			`/api/dpm/calendar/sync/${doctorID}`,
 			cookieHeader
 		);
 	} catch (e) {
-		console.error('Patients fetch failed:', e);
+		console.error('Appointments fetch failed:', e);
 	}
 
 	// DEBUGGING SECTION
-	console.warn('Patients:', patients);
+	// console.warn('Appointments:', appointment_data);
+
+	// id: time,
+	// title: reason - name
+	// description: cpr
+	// start: time
+	// end: time + 30 minutes
+	
+	//calendar.map is needed because the api returns an object with a calendar property
+	let events = appointment_data.calendar.map((appointment) => ({
+		id: appointment.time,
+		title: `${appointment.reason} - ${appointment.name}`,
+		description: appointment.cpr,
+		start: Temporal.ZonedDateTime.from(unixToZonedDateTime(appointment.time)), //convert to Temporal.ZonedDateTime
+		end: Temporal.ZonedDateTime.from(unixToZonedDateTime(appointment.time + timeAddon)) //convert to Temporal.ZonedDateTime
+	}));
+	
+	console.warn('Events:', events);
 
 
-	return { patients };
+	return { events };
 };
+
+function unixToZonedDateTime(unixSeconds: number, timeZone: string = "UTC"): string {
+  return Temporal.Instant.fromEpochMilliseconds(unixSeconds * 1000)
+    .toZonedDateTimeISO(timeZone)
+    .toString();
+}
